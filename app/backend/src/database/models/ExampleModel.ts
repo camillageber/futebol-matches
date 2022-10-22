1de09b2e0db109f38931a743111e560266f4fd57
Camilla Geber <camilla.geber@gmail.com>
2022-10-21 22:29:14 -0300

    starting project TFC :rocket:

--- app/backend/src/database/models/ExampleModel.ts
+++ app/backend/src/database/models/ExampleModel.ts
@@ -3,6 +3,7 @@ import db from '.';
 // import OtherModel from './OtherModel';
 
 class Example extends Model {
+
   // declare <campo>: <tipo>;
 }
 
@@ -17,7 +18,7 @@ Example.init({
 });
 
 /**
-  * `Workaround` para aplicar as associations em TS: 
+  * `Workaround` para aplicar as associations em TS:
   * Associations 1:N devem ficar em uma das instâncias de modelo
   * */
 
