e2064698d4b600590fa5bde51abdbaf4607565cc
Camilla Geber <camilla.geber@gmail.com>
2022-10-25 16:08:52 -0300

    creating TeamModel

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,27 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
