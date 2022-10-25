056aa8f70137a9085a3e90e0530fa89539d6f937
Camilla Geber <camilla.geber@gmail.com>
2022-10-25 16:09:06 -0300

    creating MatchModel

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,54 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import Team from './TeamModel';
+
+class Match extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  underscored: true,
+  timestamps: false,
+});
+
+Team.hasMany(Match, { foreignKey: 'id', as: 'homeTeam' });
+Team.hasMany(Match, { foreignKey: 'id', as: 'awayTeam' });
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Match;
